38b08febb00d08b7fcd5f316ff9548070bd2e1ba
“vernandaspw” <vernandaspw@gmail.com>
2022-07-28 11:06:58 +0700

    ddd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Beres Digital Laravel</title>
+    <title>Beres Digital</title>
 
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
