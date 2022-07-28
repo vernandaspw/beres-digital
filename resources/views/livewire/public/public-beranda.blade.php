a070fbfed094ff61da41242dc83ac90d27435314
“vernandaspw” <vernandaspw@gmail.com>
2022-07-28 11:05:58 +0700

    ddd

--- resources/views/livewire/public/public-beranda.blade.php
+++ resources/views/livewire/public/public-beranda.blade.php
@@ -1,6 +1,6 @@
 <div>
     <livewire:public.component.public-navbar />
-    <div class="pt-2">
+    <div class="pt-2 mb-5">
         <section class="">
             <div class="container align-items-center d-md-flex justify-content-between" style="height: 480px">
                 <div class="col-lg-6  d-md-none">
@@ -14,7 +14,7 @@
                             <b>Grow With Beres DIgital</b>
                         </div>
                         <div class="">
-                            <p>
+                            <p >
                                 Beres digital merupakan solusi membantu bagi masyarakat, pelaku bisnis UMKM dan
                                 organisasi di era digitalisasi. menyediakan produk dan jasa layanan digital. Waktunya Go
                                 Digital! #GrowWithBeresDigital
@@ -28,7 +28,41 @@
                     </div>
                 </div>
             </div>
+        </section>
 
+        <section class="mt-3" style="{{ env('bgColor400') }}">
+            <div class="container align-items-center d-md-flex justify-content-between" style="height: 280px">
+                <div class="col-lg-6 d-flex align-items-center">
+                    <div class="text-center d-flex align-items-center">
+                        <img src="{{ asset('images/logoberesdigital5.png') }}" width="300px" class="bg-transparent"
+                            alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6 d-flex align-items-center">
+                    <div class="text-white">
+                        <div class="header mb-2" style="font-size: 25px">
+                            <b>Tentang beres digital</b>
+                        </div>
+                        <div class="">
+                            <p class="" style="text-align: justify">
+                                Kami hadir sebagai solusi untuk membantu masyarakat, pelaku bisnis UMKM dan organisasi
+                                untuk meningkatkan digitalisasi, memperluas jangkauan, menyediakan produk d migital,
+                                jasa buat website, buat aplikasi, buat desain, hingga memproduksi berbagai gambar dan
+                                video untuk kebutuhan iklan dan komersial lainnya.
+                            </p>
+                            <p class="mt-2">
+                                <a href="{{ url('tentang') }}" class="">Selengkapnya >></a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="">
+            <div class="container">
+                d
+            </div>
         </section>
     </div>
 </div>
