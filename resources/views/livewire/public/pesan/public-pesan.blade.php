001166151ee87f3affd129db98a02018d0c32dec
“vernandaspw” <vernandaspw@gmail.com>
2022-08-06 09:14:22 +0700

    dd

--- resources/views/livewire/public/pesan/public-pesan.blade.php
+++ resources/views/livewire/public/pesan/public-pesan.blade.php
@@ -1,7 +1,8 @@
 <div>
     <livewire:public.component.public-navbar />
-
+    <form>
     <main class="mt-5 pt-5 mb-5 container d-flex justify-content-center">
+
         <div class="row row-cols-md-2 w-100">
             <div class="col-md-6">
                 <div class="card border-0" style="border-radius: 15px">
@@ -15,7 +16,7 @@
                             </p>
                             <hr>
                             <div class="">
-                                <form>
+
                                     <div class="">
                                         <label for="layananjenis"><b>Jenis layanan</b></label>
                                         <select class="form-control rounded-pill" id="layananjenis">
@@ -46,8 +47,9 @@
                                             <option value="">5 hari</option>
                                         </select>
                                     </div>
-                                    <hr>
+
                                     <div class="d-block d-md-none d-lg-none d-xl-none">
+                                        <hr>
                                         <div class="" style="font-weight: 600">Rinciran biaya</div>
                                         <div class="d-flex justify-content-between">
                                             <div class="">
@@ -96,71 +98,78 @@
                                                 <b>@uang(10000)</b>
                                             </div>
                                         </div>
+
+                                        <button type="submit" class="btn btn-primary rounded-pill form-control mt-3">Order
+                                            project</button>
                                     </div>
 
-                                    <button type="submit" class="btn btn-primary rounded-pill form-control mt-3">Order
-                                        project</button>
-                                </form>
+
+
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="col-md-6">
-                <div class="d-none d-md-block d-lg-block d-xl-block">
-
-                    <div class="" style="font-weight: 600">Rinciran biaya</div>
-                    <div class="d-flex justify-content-between">
-                        <div class="">
-                            Biaya dasar layanan
-                        </div>
-                        <div class="">
-                            @uang(10000)
-                        </div>
-                    </div>
-                    <br>
-                    <div class="">
-                        <div class="" style="font-weight: 600; font-size: 13px">Biaya pilihan
-                            tambahan</div>
+                <div class="d-none d-md-block d-lg-block d-xl-block card border-0" style="border-radius: 15px">
+                    <div class="card-body">
+                        <div class="" style="font-weight: 600">Rinciran biaya</div>
                         <div class="d-flex justify-content-between">
                             <div class="">
-                                Lama pengerjaan (5 Hari)
+                                Biaya dasar layanan
                             </div>
                             <div class="">
-                                @uang(4000)
+                                @uang(10000)
                             </div>
                         </div>
+                        <br>
+                        <div class="">
+                            <div class="" style="font-weight: 600; font-size: 13px">Biaya pilihan
+                                tambahan</div>
+                            <div class="d-flex justify-content-between">
+                                <div class="">
+                                    Lama pengerjaan (5 Hari)
+                                </div>
+                                <div class="">
+                                    @uang(4000)
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="d-flex justify-content-between">
+                            <div class="">
+                                Kode unik
+                            </div>
+                            <div class="">
+                                @uang(536)
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <div class="">
+                                Tax 10%
+                            </div>
+                            <div class="">
+                                @uang(1000)
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <div class="">
+                                <b>Total pembayaran</b>
+                            </div>
+                            <div class="">
+                                <b>@uang(10000)</b>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary rounded-pill form-control mt-3">Order
+                            project</button>
                     </div>
-                    <br>
-                    <div class="d-flex justify-content-between">
-                        <div class="">
-                            Kode unik
-                        </div>
-                        <div class="">
-                            @uang(536)
-                        </div>
-                    </div>
-                    <div class="d-flex justify-content-between">
-                        <div class="">
-                            Tax 10%
-                        </div>
-                        <div class="">
-                            @uang(1000)
-                        </div>
-                    </div>
-                    <div class="d-flex justify-content-between">
-                        <div class="">
-                            <b>Total pembayaran</b>
-                        </div>
-                        <div class="">
-                            <b>@uang(10000)</b>
-                        </div>
-                    </div>
-
                 </div>
             </div>
+
         </div>
+
     </main>
+</form>
 </div>
 
 <style>
