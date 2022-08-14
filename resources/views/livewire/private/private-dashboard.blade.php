e3883b0b671173720f9315f32fcaed898479d3f1
vernandaspw <vernandaspw@gmail.com>
2022-08-15 01:12:38 +0700

    ddd

--- resources/views/livewire/private/private-dashboard.blade.php
+++ resources/views/livewire/private/private-dashboard.blade.php
@@ -1,3 +1,181 @@
 <div>
     <livewire:private.component.private-navbar />
+
+    <div class="mt-3 container-fluid">
+        <div class="mb-1">
+            <h4>
+                Perlu diCek pembayaran (Ecosystem BD)
+            </h4>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <div class="card">
+                    <div class="card-header">
+                        Beres Digital
+                    </div>
+                    <div class="card-body">
+                        20
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="mb-1">
+            <h4>
+                Beres digital Project
+            </h4>
+        </div>
+        <div class="row">
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        Semua
+                    </div>
+                    <div class="card-body">
+                        20
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        pending
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        approve
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        pay awal
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        pay awal true
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        mulai pengerjaan
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        selesai dikerjakan
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        review
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        revisi
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        pay akhir
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        pay akhir true
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        penyerahan
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        selesai
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        batal
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 my-1">
+                <div class="card">
+                    <div class="card-header">
+                        tolak
+                    </div>
+                    <div class="card-body">
+                        2
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
