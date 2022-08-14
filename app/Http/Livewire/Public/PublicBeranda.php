5ae283c919071a2f7c742680a0247f1d2dd2fc07
vernandaspw <vernandaspw@gmail.com>
2022-08-15 01:01:03 +0700

    ddd

--- app/Http/Livewire/Public/PublicBeranda.php
+++ app/Http/Livewire/Public/PublicBeranda.php
@@ -3,17 +3,21 @@
 namespace App\Http\Livewire\Public;
 
 use App\Models\Layanan;
+use App\Models\Portofolio;
 use App\Models\Produk;
 use Livewire\Component;
 
 class PublicBeranda extends Component
 {
-    public $produk, $layanan;
+    public $produk, $layanan, $porto;
+
+    public $take = 16;
 
     public function render()
     {
         $this->produk = Produk::where('istersedia', true)->orderBy('urut', "DESC")->get();
         $this->layanan = Layanan::where('istersedia', true)->orderBy('urut', "DESC")->get();
+        $this->porto = Portofolio::with('layananjenis', 'layanan')->where('istampil', true)->take($this->take)->get();
         return view('livewire.public.public-beranda')->extends('layouts.app')->section('content');
     }
 }
